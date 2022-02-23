3b3f7cc684790c522f74c5e950f2487d7c8bd22e
Chetan <chetan.verma_cs19@gla.ac.in>
2022-02-23 23:13:58 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Switch>
+      <Route path="/" exact>
+        <h1>Hello! This is home page.</h1>
+      </Route>
+
+      <Route path="/contact" exact>
+        <h1>Hello! This is contact page.</h1>
+      </Route>
+
+      <Route>
+        <h1>404 Page not found!!!</h1>
+      </Route>
+    </Switch>
   );
 }
 
